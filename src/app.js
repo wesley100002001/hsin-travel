881c563ea818c3d24bf4951b17b5e2e9f4c583fb
Wesley <wesley100002001@gmail.com>
2016-08-10 21:26:08 +0800

    Eliminate unused component for hsin project

--- src/app.js
+++ src/app.js
@@ -8,15 +8,14 @@ import firebase from 'angularfire';
 
 import home from './app/features/home';
 import login from './app/features/login';
-import members from './app/features/members';
-import order from './app/features/order';
-import orderexport from './app/features/orderexport';
-import orderimport from './app/features/orderimport';
-import orders from './app/features/orders';
-import password from './app/features/password';
+// import members from './app/features/members';
+// import order from './app/features/order';
+// import orderexport from './app/features/orderexport';
+// import orderimport from './app/features/orderimport';
+// import orders from './app/features/orders';
+// import password from './app/features/password';
 import requests from './app/features/requests';
 import routing from './app.config';
 
-angular.module('app', [firebase, uirouter, ngCookies, home, login, members, order,
-  orderexport, orderimport, orders, password, requests])
+angular.module('app', [firebase, uirouter, ngCookies, home, login, requests])
   .config(routing);
