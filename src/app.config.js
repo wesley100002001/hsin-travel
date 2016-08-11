1ccce973431d65a40c0e0796d390965bfb31bc87
ychi <gruenen@gmail.com>
2016-08-11 17:17:32 +0800

    add app.config.js back

---
+++ src/app.config.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import requests from './app/reducers/index';
+
+routes.$inject = ['$urlRouterProvider', '$locationProvider', '$ngReduxProvider'];
+
+export default function routes($urlRouterProvider, $locationProvider, $ngReduxProvider) {
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise('/');
+  let reducer = combineReducers({requests});
+  $ngReduxProvider.createStoreWith(reducer, []);
+}
