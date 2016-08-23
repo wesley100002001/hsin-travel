690cff157c7b816d017c20420d4622087e12a8f3
ychi <gruenen@gmail.com>
2016-08-23 22:51:16 +0800

    add translation framework

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,23 @@
 import { combineReducers } from 'redux';
 import requests from './app/reducers/index';
+//!! Not Sure how to load these objects yet
+import en from './translations/en';
+import jp from './translations/jp'
 
-routes.$inject = ['$urlRouterProvider', '$locationProvider', '$ngReduxProvider'];
+routes.$inject = ['$urlRouterProvider', '$locationProvider', '$ngReduxProvider', '$translateProvider'];
 
-export default function routes($urlRouterProvider, $locationProvider, $ngReduxProvider) {
+export default function routes($urlRouterProvider, $locationProvider, $ngReduxProvider, $translateProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
+  var tw = {
+    Login: '登入',
+    Home: '首頁',
+    Requests: '需求列表'
+  };
+
+  $translateProvider.translations('tw', tw)
+  .preferredLanguage('tw');
+
   let reducer = combineReducers({requests});
   $ngReduxProvider.createStoreWith(reducer, []);
 }
