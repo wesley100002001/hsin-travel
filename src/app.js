08b07e838c6faeb898354d4383ab3f76067620e8
Wesley <wesley100002001@gmail.com>
2016-08-29 15:27:30 +0800

    Remove unused features

--- src/app.js
+++ src/app.js
@@ -10,11 +10,6 @@ import 'angular-translate';
 
 import home from './app/features/home';
 import login from './app/features/login';
-// import members from './app/features/members';
-// import order from './app/features/order';
-// import orderexport from './app/features/orderexport';
-// import orderimport from './app/features/orderimport';
-// import orders from './app/features/orders';
 // import password from './app/features/password';
 import request from './app/features/request';
 import requests from './app/features/requests';
