37d220f2985bd7356169537070dec028a373e828
Wesley <wesley100002001@gmail.com>
2016-12-05 09:53:08 +0800

    Get request title from RESTful

--- src/app/services/restful.service.js
+++ src/app/services/restful.service.js
@@ -101,6 +101,7 @@ class Restful {
   getMockRequest () {
     return this.$q(function (resolve, reject) {
       var request = {
+        title: '花東縱谷七天六夜',
         hotel: [
           { date: '2016/09/20', hotelId: 'H003', hotelName: '國賓大飯店', roomId: 13, roomTitle: '雙人房', quantity: 20 },
           { date: '2016/09/21', hotelId: 'H003', hotelName: '國賓大飯店', roomId: 13, roomTitle: '雙人房', quantity: 20 },
