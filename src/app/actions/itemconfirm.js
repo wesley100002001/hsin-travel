33086cfb1b1b74665af0dee36b55efa8c7b27939
Wesley <wesley100002001@gmail.com>
2017-01-10 22:44:00 +0800

    Add an action: submitAccomodation

--- src/app/actions/itemconfirm.js
+++ src/app/actions/itemconfirm.js
@@ -1,7 +1,9 @@
 import moment from 'moment';
+import { fetchRequest } from './discuss';
 import * as restful from '../lib/restful';
 
 export const FETCH_ACCO = 'FETCH_ACCO';
+export const PATCH_ACCO = 'PATCH_ACCO';
 
 export function fetchAccomodation (accoId) {
   return {
@@ -12,4 +14,26 @@ export function fetchAccomodation (accoId) {
         return response;
       })
   };
+}
+
+export function submitAccomodation (acco, requestId) {
+  return dispatch => {
+    return new Promise((resolve, reject) => {
+      resolve(dispatch(updateAccomodation(acco)));
+    }).then(response => {
+      // 因為無法在 modal 關掉之後 trigger action 去 fetch request
+      // 所以先 fetch
+      return new Promise((resolve, reject) => {
+        resolve(dispatch(fetchRequest(requestId)));
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  };
+}
+
+export function updateAccomodation (acco) {
+  return {
+    type: PATCH_ACCO,
+  }
 }
