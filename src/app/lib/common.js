19305a4bb2c36e0296f9cc35e876c48d8fa969a9
Wesley <wesley100002001@gmail.com>
2017-03-12 10:20:47 +0800

    Add common.js for common functions

---
+++ src/app/lib/common.js
@@ -0,0 +1,14 @@
+import moment from 'moment';
+
+const JPWeek = ['(日)', '(月)', '(火)', '(水)', '(木)', '(金)', '(土)'];
+
+export function getJPDate (date) {
+  return moment(date).format('YYYY-MM-DD')
+    .concat(JPWeek[moment(date).day()]);
+}
+
+export function getJPDateWithHour (date) {
+  return moment(date).format('YYYY-MM-DD')
+    .concat(JPWeek[moment(date).day()])
+    .concat(moment(date).format('HH:mm'));
+}
